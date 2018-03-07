07f97248b4772cde41cbb45e099f7c991b3fb6c2
George Antonious <george@antonious.ca>
2018-03-07 14:45:42 -0700

    Add factory for building dialog flow api

---
+++ DialogFlow.Sdk/Services/DialogFlowConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace DialogFlow.Sdk.Services
+{
+    public class DialogFlowConfig
+    {
+        public string BaseApiUrl { get; set; } = "https://api.dialogflow.com/v1/";
+        public string ApiKey { get; set; }
+    }
+}
---
+++ DialogFlow.Sdk/Services/DialogFlowServiceFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using DialogFlow.Sdk.Services;
+using Refit;
+
+namespace DialogFlow.Sdk.Services
+{
+    public class DialogFlowServiceFactory
+    {
+        private readonly HttpClient _httpClient;
+
+        public DialogFlowServiceFactory(DialogFlowConfig dialogFlowConfig)
+        {
+            _httpClient = BuildAuthenticatingHttpClientFrom(dialogFlowConfig);
+        }
+     
+        public IDialogFlowService CreateDialogFlowService()
+        {
+            return RestService.For<IDialogFlowService>(_httpClient);
+        }
+
+        private static HttpClient BuildAuthenticatingHttpClientFrom(DialogFlowConfig dialogFlowConfig)
+        {
+            return new HttpClient(new AuthenticatingHttpClientHandler(dialogFlowConfig.ApiKey))
+            {
+                BaseAddress = new Uri(dialogFlowConfig.BaseApiUrl)
+            };
+        }
+    }
+
+    public class AuthenticatingHttpClientHandler : HttpClientHandler
+    {
+        private readonly string _apiToken;
+
+        public AuthenticatingHttpClientHandler(string apiToken)
+        {
+            _apiToken = apiToken;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("Authorization", $"Bearer {_apiToken}");
+            return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}

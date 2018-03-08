fbd087562e736bfaa4436d2034b001db60c5ff49
calho <clho@ualberta.ca>
2018-03-07 17:10:54 -0700

    some Jobber API stuff?

---
+++ Jobber.Sdk/Models/JobberModificationResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jobber.Sdk.Models
+{
+    public class JobberModificationResponse
+    {
+        public string Id { get; set; }
+        public JobberStatus Status { get; set; }
+    }
+
+    public class JobberStatus
+    {
+        public int Code { get; set; }
+        public string Error { get; set; }
+    }
+}

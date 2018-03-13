01eb53c667ef9cc8595dfd391205b4c2b08531d2
George Antonious <george@antonious.ca>
2018-03-13 16:10:32 -0600

    Add JobberServiceFactory

---
+++ Jobber.Sdk/JobberServiceFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Refit;
+
+namespace Jobber.Sdk
+{
+    public class JobberServiceFactory
+    {
+        public IJobberService CreateDialogFlowService(JobberConfig jobberConfig)
+        {
+            var httpClient = BuildAuthenticatingHttpClientFrom(jobberConfig);
+            return RestService.For<IJobberService>(httpClient);
+        }
+
+        private static HttpClient BuildAuthenticatingHttpClientFrom(JobberConfig jobberConfig)
+        {
+            return new HttpClient(new AuthenticatingHttpClientHandler(jobberConfig.ApiKey))
+            {
+                BaseAddress = new Uri(jobberConfig.BaseApiUrl)
+            };
+        }
+    }
+
+    public class AuthenticatingHttpClientHandler : HttpClientHandler
+    {
+        private readonly string _apiToken;
+
+        public AuthenticatingHttpClientHandler(string apiToken)
+        {
+            _apiToken = apiToken;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("Authorization", $"Bearer {_apiToken}");
+            return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+        }
+    }
+    }
+}

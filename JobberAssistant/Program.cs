1c96f2ae5637441c9f14bd1947ccd1dd2d4b23d0
George Antonious <george@antonious.ca>
2018-03-13 15:49:38 -0600

    Add JobberAssistant project and up all project from c# 7.0 -> 7.2

---
+++ JobberAssistant/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace JobberAssistant
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}

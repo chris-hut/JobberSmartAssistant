39a166bcb7f1dc35a225e7864fd7ab5f6f1a45b6
George Antonious <george@antonious.ca>
2018-03-13 23:34:03 -0600

    Add more builder for fulfillment

---
+++ DialogFlow.Sdk/Builders/ContextBuilder.cs
@@ -0,0 +1,40 @@
+﻿using DialogFlow.Sdk.Intents;
+
+namespace DialogFlow.Sdk.Builders
+{
+    public class ContextBuilder
+    {
+        private readonly Context _context;
+
+        private ContextBuilder(string name)
+        {
+            _context = new Context
+            {
+                Name = name,
+                Lifespan = 3
+            };
+        }
+
+        public ContextBuilder For(string name)
+        {
+            return new ContextBuilder(name);
+        }
+
+        public ContextBuilder Lifespan(int lifespan)
+        {
+            _context.Lifespan = lifespan;
+            return this;
+        }
+
+        public ContextBuilder WithParameter(string key, string value)
+        {
+            _context.Parameters[key] = value;
+            return this;
+        }
+
+        public Context Build()
+        {
+            return _context;
+        }
+    }
+}
---
+++ DialogFlow.Sdk/Builders/FulfillmentResponseBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using DialogFlow.Sdk.Fulfillment;
+using DialogFlow.Sdk.Intents;
+
+namespace DialogFlow.Sdk.Builders
+{
+    public class FulfillmentResponseBuilder
+    {
+        private readonly FulfillmentResponse _fulfillmentResponse;
+
+        private FulfillmentResponseBuilder()
+        {
+            _fulfillmentResponse = new FulfillmentResponse();
+        }
+
+        public static FulfillmentResponseBuilder Create()
+        {
+            return new FulfillmentResponseBuilder();
+        }
+
+        public FulfillmentResponseBuilder Speech(string speech)
+        {
+            _fulfillmentResponse.Speech = speech;
+            return this;
+        }
+
+        public FulfillmentResponseBuilder DisplayText(string displayText)
+        {
+            _fulfillmentResponse.DisplayText = displayText;
+            return this;
+        }
+
+        public FulfillmentResponseBuilder WithContext(ContextBuilder contextBuilder)
+        {
+            _fulfillmentResponse.ContextOut.Add(contextBuilder.Build());
+            return this;
+        }
+        
+        public FulfillmentResponse Build()
+        {
+            if (String.IsNullOrEmpty(_fulfillmentResponse.DisplayText))
+            {
+                _fulfillmentResponse.DisplayText = _fulfillmentResponse.Speech;
+            }
+            
+            return _fulfillmentResponse;
+        }
+    }
+}

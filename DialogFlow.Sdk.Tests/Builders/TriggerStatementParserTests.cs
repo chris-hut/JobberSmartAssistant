6f2fcbe52518cecdea0f5db484c60560eb87bb14
George Antonious <george@antonious.ca>
2018-03-15 18:54:48 -0600

    Move extensions to utils namespace

--- DialogFlow.Sdk.Tests/Builders/TriggerStatementParserTests.cs
+++ DialogFlow.Sdk.Tests/Builders/TriggerStatementParserTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 using DialogFlow.Sdk.Builders;
 using DialogFlow.Sdk.Intents;
+using DialogFlow.Sdk.Tests.Utils;
 using NUnit.Framework;
 
 namespace DialogFlow.Sdk.Tests.Builders
@@ -17,24 +18,48 @@ namespace DialogFlow.Sdk.Tests.Builders
             Assert.AreEqual(1, parsedStatement.Data.Count);
             parsedStatement.Data[0].AssertIsEntityDataWith("@sys.any", "amount", "6");
         }
+        
+        [Test]
+        public void TestOnlyEntityDataWithSpace()
+        {
+            var statement = "[@sys.any:name:John Applessed]";
+            var parsedStatement = new TriggerStatementParser().Parse(statement);
+            
+            Assert.AreEqual(1, parsedStatement.Data.Count);
+            parsedStatement.Data[0].AssertIsEntityDataWith("@sys.any", "name", "John Applessed");
+        }
+        
+        [Test]
+        public void TestOnlyTextData()
+        {
+            var statement = "What's the weather like?";
+            var parsedStatement = new TriggerStatementParser().Parse(statement);
+            
+            Assert.AreEqual(1, parsedStatement.Data.Count);
+            parsedStatement.Data[0].AssertIsTextDataWith("What's the weather like?");
+        }
+
+        [Test]
+        public void TestEmbeddedEntityData()
+        {
+            var statement = "Can you call [@sys.any:name:John Applessed] please?";
+            var parsedStatement = new TriggerStatementParser().Parse(statement);
+            
+            Assert.AreEqual(3, parsedStatement.Data.Count);
+            parsedStatement.Data[0].AssertIsTextDataWith("Can you call ");
+            parsedStatement.Data[1].AssertIsEntityDataWith("@sys.any", "name", "John Applessed");
+            parsedStatement.Data[2].AssertIsTextDataWith(" please?");
+        }
+
+        [Test]
+        public void TestTextEndingWithEntityData()
+        {
+            var statement = "Call [@sys.any:name:John Applessed]";
+            var parsedStatement = new TriggerStatementParser().Parse(statement);
+            
+            Assert.AreEqual(2, parsedStatement.Data.Count);
+            parsedStatement.Data[0].AssertIsTextDataWith("Call ");
+            parsedStatement.Data[1].AssertIsEntityDataWith("@sys.any", "name", "John Applessed");
+        }
     }
-
-    internal static class IConversationDataExtensions
-    {
-        public static void AssertIsTextDataWith(this IConversationData conversationData, string text)
-        {
-            Assert.IsTrue(conversationData is TextData);
-            var textData = (TextData) conversationData;
-            Assert.AreEqual(text, textData.Text);
-        }
-
-        public static void AssertIsEntityDataWith(this IConversationData conversationData, string meta, string alias, string text)
-        {
-            Assert.IsTrue(conversationData is EntityData);
-            var entityData = (EntityData) conversationData;
-            Assert.AreEqual(alias, entityData.Alias);
-            Assert.AreEqual(meta, entityData.Meta);
-            Assert.AreEqual(text, entityData.Text);
-        }
-    } 
 }
---
+++ DialogFlow.Sdk.Tests/Utils/IConversationDataExtensions.cs
@@ -0,0 +1,24 @@
+﻿using DialogFlow.Sdk.Intents;
+using NUnit.Framework;
+
+namespace DialogFlow.Sdk.Tests.Utils
+{
+    public static class IConversationDataExtensions
+    {
+        public static void AssertIsTextDataWith(this IConversationData conversationData, string text)
+        {
+            Assert.IsTrue(conversationData is TextData);
+            var textData = (TextData) conversationData;
+            Assert.AreEqual(text, textData.Text);
+        }
+
+        public static void AssertIsEntityDataWith(this IConversationData conversationData, string meta, string alias, string text)
+        {
+            Assert.IsTrue(conversationData is EntityData);
+            var entityData = (EntityData) conversationData;
+            Assert.AreEqual(alias, entityData.Alias);
+            Assert.AreEqual(meta, entityData.Meta);
+            Assert.AreEqual(text, entityData.Text);
+        }
+    } 
+}

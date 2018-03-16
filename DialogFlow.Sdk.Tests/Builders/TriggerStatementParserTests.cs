d8cfb49813044f58b4820d78b6aaf52a108c0632
George Antonious <george@antonious.ca>
2018-03-15 18:41:05 -0600

    Add some tests

--- DialogFlow.Sdk.Tests/Builders/TriggerStatementParserTests.cs
+++ DialogFlow.Sdk.Tests/Builders/TriggerStatementParserTests.cs
@@ -1,10 +1,19 @@
-﻿using NUnit.Framework;
+﻿using DialogFlow.Sdk.Builders;
+using NUnit.Framework;
 
 namespace DialogFlow.Sdk.Tests.Builders
 {
     [TestFixture]
     public class TriggerStatementParserTests
     {
-        
+        [Test]
+        public void Test()
+        {
+            var statement = "[@sys.any:amount:6]";
+            var parsedStatement = new TriggerStatementParser().Parse(statement);
+            
+            Assert.AreEqual(1, parsedStatement.Count);
+            
+        }
     }
 }

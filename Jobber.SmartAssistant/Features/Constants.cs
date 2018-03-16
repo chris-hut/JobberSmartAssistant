5e93137c1689b75d693c3c9238d38b3c43fd9c03
George Antonious <george@antonious.ca>
2018-03-15 18:05:43 -0600

    Start working on client set intent

--- Jobber.SmartAssistant/Features/Constants.cs
+++ Jobber.SmartAssistant/Features/Constants.cs
@@ -5,7 +5,6 @@
         public static readonly string ASSISTANT_NAME = "Jobber Bot";
         
         public static readonly string START_CREATE_JOB = "START_CREATE_JOB";
-        public static readonly string CLIENT_REQUESTED_CREATE_JOB = "CLIENT_REQUESTED_CREATE_JOB";
         public static readonly string CLIENT_SET_CREATE_JOB = "CLIENT_SET_CREATE_JOB";
 
         public static readonly string CLIENT_VAR = "CLIENT";
---
+++ Jobber.SmartAssistant/Features/CreateJob/ClientSetCreateJobintentDefintion.cs
@@ -0,0 +1,17 @@
+﻿using Assistant.Sdk.BuiltIns;
+using DialogFlow.Sdk.Builders;
+using DialogFlow.Sdk.Intents;
+
+namespace Jobber.SmartAssistant.Features.CreateJob
+{
+    public class ClientSetCreateJobintentDefintion : IIntentDefinition
+    {
+        public Intent DefineIntent()
+        {
+            return IntentBuilder.For(Constants.CLIENT_SET_CREATE_JOB)
+                .RequiresContext(Constants.START_CREATE_JOB)
+                .FulfillWithWebhook()
+                .Build();
+        }
+    }
+}

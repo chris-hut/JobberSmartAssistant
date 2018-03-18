bd4cb041df984c92e8ed09a0148e10a22a437d62
George Antonious <george@antonious.ca>
2018-03-18 15:39:22 -0600

    Add jobber exceptions

---
+++ Jobber.Sdk/Rest/JobberException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Jobber.Sdk.Rest
+{
+    public class JobberException : Exception
+    {
+        public JobberException(string errorMessage, string errorCause) :
+            base(BuildExceptionMessageFrom(errorMessage, errorCause)) { }
+
+        private static string BuildExceptionMessageFrom(string errorMessage, string errorCause)
+        {
+            return $"{errorMessage} because of:\n{errorCause}";
+        }
+    }
+}

2e11379cf404e67b774bd3c8c17657d3c06c0c0f
George Antonious <george@antonious.ca>
2018-03-19 23:37:23 -0600

    Test simple image url

--- Jobber.SmartAssistant/Features/CreateJob/DescriptionRequestedCreateJobIntentFulfiller.cs
+++ Jobber.SmartAssistant/Features/CreateJob/DescriptionRequestedCreateJobIntentFulfiller.cs
@@ -62,7 +62,7 @@ namespace Jobber.SmartAssistant.Features.CreateJob
             return GoogleCardBuilder.Create()
                 .Title($"New Job for {createJobContext.Client.Name}")
                 .Content(description)
-                .Image(mapImage, "Map of job location.")
+                .Image("http://www.antonious.ca/media/icons/github-1024.png", "Map of job location.")
                 .WithButton("Open Map", mapLink)
                 .Build();
         }

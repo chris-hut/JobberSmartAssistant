6e71bcb6f01f4263d0e68c12c36582825c9e2547
calho <clho@ualberta.ca>
2018-03-23 17:31:51 -0600

    adding more training for that google

--- Jobber.SmartAssistant/Features/GetRevenue/GetRevenueIntentDefiniton.cs
+++ Jobber.SmartAssistant/Features/GetRevenue/GetRevenueIntentDefiniton.cs
@@ -12,12 +12,15 @@ namespace Jobber.SmartAssistant.Features.GetRevenue
                .TriggerOn("Get revenue")
                .TriggerOn("Get revenue from last week")
                .TriggerOn("Get revenue from last month")
+               .TriggerOn("Get revenue from last year")
                .TriggerOn("How much money we made")
                .TriggerOn("How much money we made last week")
                .TriggerOn("How much money we made last month")
+               .TriggerOn("How much money we made last year")
                .TriggerOn("How much were we paid")
                .TriggerOn("How much were we paid last week")
                .TriggerOn("How much were we paid last month")
+               .TriggerOn("How much were we paid last year")
                .FulfillWithWebhook()
                .Build();
         }

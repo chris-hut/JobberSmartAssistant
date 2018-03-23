2ebdd7c4a9317704456c55e5235eb31a00b7c23b
George Antonious <george@antonious.ca>
2018-03-22 19:14:30 -0600

    Start GetCompletableVisits feature

---
+++ Jobber.SmartAssistant/Features/GetCompletableVisits/GetCompletableVisitsIntentDefinition.cs
@@ -0,0 +1,21 @@
+﻿using Assistant.Sdk.BuiltIns;
+using DialogFlow.Sdk.Builders;
+using DialogFlow.Sdk.Models.Intents;
+
+namespace Jobber.SmartAssistant.Features.GetCompletableVisits
+{
+    public class GetCompletableVisitsIntentDefinition : IIntentDefinition
+    {
+        public Intent DefineIntent()
+        {
+            return IntentBuilder.For(Constants.Intents.GetCompletableVisits)
+                .TriggerOn("How many visits can be marked as complete?")
+                .TriggerOn("Completable visits")
+                .TriggerOn("How many visits can I complete")
+                .TriggerOn("How many visits need to be completed")
+                .TriggerOn("How many visits are ready for completion")
+                .FulfillWithWebhook()
+                .Build();
+        }
+    }
+}

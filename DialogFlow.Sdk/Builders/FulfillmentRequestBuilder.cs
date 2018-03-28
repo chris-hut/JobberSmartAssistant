f7638c8f3ca92b3b1304083fa533879adacb3af5
George Antonious <george@antonious.ca>
2018-03-28 16:08:40 -0600

    Add FulfillmentRequestBuilder for testing

---
+++ DialogFlow.Sdk/Builders/FulfillmentRequestBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using DialogFlow.Sdk.Models.Common;
+using DialogFlow.Sdk.Models.Fulfillment;
+
+namespace DialogFlow.Sdk.Builders
+{
+    public class FulfillmentRequestBuilder
+    {
+        private readonly FulfillmentRequest _fulfillmentRequest;
+
+        private FulfillmentRequestBuilder(string name)
+        {
+            _fulfillmentRequest = new FulfillmentRequest
+            {
+                ConversationResult = new ConversationResult {ActionName = name}
+            };
+        }
+
+        public static FulfillmentRequestBuilder For(string name)
+        {
+            return new FulfillmentRequestBuilder(name);
+        }
+
+        public FulfillmentRequestBuilder WithParameter(string parameterName, object parameterValue)
+        {
+            _fulfillmentRequest.ConversationResult.Parameters[parameterName] = parameterName.ToString();
+            return this;
+        }
+
+        public FulfillmentRequestBuilder WithContextParamter(string contextName, string parameterName, object parameterValue)
+        {
+            _fulfillmentRequest.ConversationResult.Contexts.Add(new Context
+            {
+                Name = contextName,
+                Parameters = new Dictionary<string, string>
+                {
+                    { parameterName, parameterValue.ToString() }
+                }
+            });
+            return this;
+        }
+
+        public FulfillmentRequest Build()
+        {
+            return _fulfillmentRequest;
+        }
+    }
+}

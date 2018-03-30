34a64c2238be203c3b443cd28767e00b6bfd6612
George Antonious <george@antonious.ca>
2018-03-30 10:42:54 -0600

    Create more elegant MockJobberClientBuilder for defining a JobberMock

--- Jobber.SmartAssistant.Tests/Features/CreateJob/ClientRequestedCreateJobIntentFulfillerTests.cs
+++ Jobber.SmartAssistant.Tests/Features/CreateJob/ClientRequestedCreateJobIntentFulfillerTests.cs
@@ -1,12 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using DialogFlow.Sdk.Builders;
-using DialogFlow.Sdk.Models.Fulfillment;
-using Jobber.Sdk;
 using Jobber.Sdk.Models.Clients;
 using Jobber.SmartAssistant.Features;
 using Jobber.SmartAssistant.Features.CreateJob;
 using Jobber.SmartAssistant.Tests.Extensions;
-using Moq;
+using Jobber.SmartAssistant.Tests.Mocks;
 using NUnit.Framework;
 
 namespace Jobber.SmartAssistant.Tests.Features.CreateJob
@@ -17,19 +16,16 @@ namespace Jobber.SmartAssistant.Tests.Features.CreateJob
         [TestCase]
         public async Task TestIfNoClient()
         {
-            var jobberClientMock = new Mock<IJobberClient>();
-            jobberClientMock
-                .Setup(j => j.GetClientsAsync(It.IsAny<string>()))
-                .ReturnsAsync(() => new ClientCollection());
-
-            var jobberClient = jobberClientMock.Object;
+            var mockJobberClient = MockJobberClientBuilder.Create()
+                .ReturnsClients(new List<Client>())
+                .Build();
 
             var fulfillmentRequest = FulfillmentRequestBuilder.Create(Constants.Intents.ClientRequestedCreateJob)
                 .WithParameter(Constants.Variables.ClientName, "George")
                 .Build();
             
             var fulfiller = new ClientRequestedCreateJobIntentFulfiller();
-            var response = await fulfiller.FulfillAsync(fulfillmentRequest, jobberClient);
+            var response = await fulfiller.FulfillAsync(fulfillmentRequest, mockJobberClient);
             
             response.AssertResponseSpeech("Sorry I dont know who George is.");
         }
---
+++ Jobber.SmartAssistant.Tests/Mocks/MockJobberClientBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Jobber.Sdk;
+using Jobber.Sdk.Models.Clients;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Moq;
+
+namespace Jobber.SmartAssistant.Tests.Mocks
+{
+    public class MockJobberClientBuilder
+    {
+        private readonly Mock<IJobberClient> _jobberClientMock;
+
+        private MockJobberClientBuilder()
+        {
+            _jobberClientMock = new Mock<IJobberClient>();
+        }
+
+        public static MockJobberClientBuilder Create()
+        {
+            return new MockJobberClientBuilder();
+        }
+
+        public MockJobberClientBuilder ReturnsClients(IEnumerable<Client> clients)
+        {
+            _jobberClientMock
+                .Setup(m => m.GetClientsAsync(It.IsAny<string>()))
+                .ReturnsAsync(() => new ClientCollection
+                {
+                    Clients = clients
+                });
+            
+            return this;
+        }
+        
+        public IJobberClient Build()
+        {
+            return _jobberClientMock.Object;
+        }
+    }
+}

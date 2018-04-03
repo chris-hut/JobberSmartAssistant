f151c3fe26ffe0f41660018093ecb537c36c95a8
George Antonious <george@antonious.ca>
2018-04-03 13:54:27 -0600

    Add more trigger statements to unassigned visits

--- Jobber.SmartAssistant/Features/UnassignedVisits/UnassignedVisitsIntentDefinition.cs
+++ Jobber.SmartAssistant/Features/UnassignedVisits/UnassignedVisitsIntentDefinition.cs
@@ -10,9 +10,14 @@ namespace Jobber.SmartAssistant.Features.UnassignedVisits
         {
             return IntentBuilder.For(Constants.Intents.UnassignedVisits)
                 .TriggerOn("How many visits do I still need to assign today?")
+                .TriggerOn("How many visits do I still need to assign?")
                 .TriggerOn("How many visits are unassigned today?")
+                .TriggerOn("Can you let me know how many visits I still need to assign today?")
+                .TriggerOn("Can you let me know how many visits I need to assign today?")
+                .TriggerOn("Can you tell me know how many visits I still need to assign today?")
                 .TriggerOn("Unassigned visits for today")
                 .TriggerOn("Unassigned visits")
+                .TriggerOn("Get unassigned visits")
                 .FulfillWithWebhook()
                 .Build();
         }

2b7d8d08a71eb7f75f202128b8656a2ced00f12c
George Antonious <george@antonious.ca>
2018-04-03 14:44:19 -0600

    Add more trigger statements to get next job

--- Jobber.SmartAssistant/Features/GetNextVisit/GetNextVisitIntentDefinition.cs
+++ Jobber.SmartAssistant/Features/GetNextVisit/GetNextVisitIntentDefinition.cs
@@ -10,6 +10,13 @@ namespace Jobber.SmartAssistant.Features.GetNextVisit
         {
             return IntentBuilder.For(Constants.Intents.GetNextVisit)
                 .TriggerOn("What is my next visit?")
+                .TriggerOn("What is my next job?")
+                .TriggerOn("When is my next job?")
+                .TriggerOn("When is my next visit?")
+                .TriggerOn("What do I have next?")
+                .TriggerOn("Can you tell me about my next job?")
+                .TriggerOn("Can you tell me about my next visit?")
+                .TriggerOn("Can you tell me a bit about my next visit?")
                 .TriggerOn("Get my next visit.")
                 .FulfillWithWebhook()
                 .Build();

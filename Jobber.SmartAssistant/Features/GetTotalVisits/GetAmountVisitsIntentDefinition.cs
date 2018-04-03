819d058ca398be1f27eba282e65ac6c5078fea90
George Antonious <george@antonious.ca>
2018-04-03 14:40:26 -0600

    Add trigger statements to GetAmountVisits

--- Jobber.SmartAssistant/Features/GetTotalVisits/GetAmountVisitsIntentDefinition.cs
+++ Jobber.SmartAssistant/Features/GetTotalVisits/GetAmountVisitsIntentDefinition.cs
@@ -9,8 +9,18 @@ namespace Jobber.SmartAssistant.Features.GetTotalVisits
         public Intent DefineIntent()
         {
             return IntentBuilder.For(Constants.Intents.GetAmountVisits)
+                .TriggerOn("How many visits do I have?")
                 .TriggerOn("How many visits do I have today?")
-                .TriggerOn("How many work for me today.")
+                .TriggerOn("How many sites do I need to visit today?")
+                .TriggerOn("How many sites do I need to visit?")
+                .TriggerOn("How many jobs do I have today?")
+                .TriggerOn("How many jobs do I have?")
+                .TriggerOn("Could you let me know how many places I need to go to today?")
+                .TriggerOn("Could you let me know how many jobs I have?")
+                .TriggerOn("Could you let me know how many visits I have?")
+                .TriggerOn("Could you let me know how many visits I have today?")
+                .TriggerOn("Can you let me know how many jobs I have?")
+                .TriggerOn("Get todays visits")
                 .FulfillWithWebhook()
                 .Build();
         }   

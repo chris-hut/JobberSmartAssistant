f5ca9b7a182eec7cb5b42bfe0ca93e9841ddce57
George Antonious <george@antonious.ca>
2018-04-03 12:04:38 -0600

    Add additional trigger statements for convertible quotes

--- Jobber.SmartAssistant/Features/ConvertableQuotes/ConvertibleQuoteIntentDefinition.cs
+++ Jobber.SmartAssistant/Features/ConvertableQuotes/ConvertibleQuoteIntentDefinition.cs
@@ -1,6 +1,5 @@
 ﻿using Assistant.Sdk.BuiltIns;
 using DialogFlow.Sdk.Builders;
-using DialogFlow.Sdk.Models.Common;
 using DialogFlow.Sdk.Models.Intents;
 
 namespace Jobber.SmartAssistant.Features.ConvertableQuotes
@@ -10,15 +9,18 @@ namespace Jobber.SmartAssistant.Features.ConvertableQuotes
         public Intent DefineIntent()
         {
             return IntentBuilder.For(Constants.Intents.ConvertibleQuotes)
-               .TriggerOn("How many quotes are ready to be turned into jobs?")
-               .TriggerOn("How many quotes are ready?")
-               .TriggerOn("How many quotes are approved?")
-               .TriggerOn("How many quotes can become jobs?")
-               .TriggerOn("Ready quotes")
-               .TriggerOn("Convertible quotes")
-               .TriggerOn("approved quotes")
-               .FulfillWithWebhook()
-               .Build();
+                .TriggerOn("How many quotes are ready to be turned into jobs?")
+                .TriggerOn("How many quotes are ready?")
+                .TriggerOn("How many quotes are approved?")
+                .TriggerOn("How many quotes can become jobs?")
+                .TriggerOn("Ready quotes")
+                .TriggerOn("Convertible quotes")
+                .TriggerOn("Approved quotes")
+                .TriggerOn("Convertible quotes")
+                .TriggerOn("Can you let me know how many quotes are ready to be turned into jobs?")
+                .TriggerOn("I wonder how many quotes are approved")
+                .FulfillWithWebhook()
+                .Build();
         }
     }
 }

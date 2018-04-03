4a48e790b113359b77dbedf206f4717411418acf
George Antonious <george@antonious.ca>
2018-04-03 12:33:10 -0600

    Add more trigger statements to getting revenue

--- Jobber.SmartAssistant/Features/GetRevenue/GetRevenueIntentDefiniton.cs
+++ Jobber.SmartAssistant/Features/GetRevenue/GetRevenueIntentDefiniton.cs
@@ -10,16 +10,21 @@ namespace Jobber.SmartAssistant.Features.GetRevenue
         public Intent DefineIntent()
         {
             return IntentBuilder.For(Constants.Intents.GetRevenue)
-               .TriggerOn("Get revenue")
-               .TriggerOn($"Get revenue from [{Entity.DatePeriod}:{Constants.Variables.TimeUnit}:month]")
-               .TriggerOn("How much money have we made")
-               .TriggerOn($"How much money have we made in [{Entity.DatePeriod}:{Constants.Variables.TimeUnit}:month]")
-               .TriggerOn("How much were we paid")
-               .TriggerOn($"How much were we paid in [{Entity.DatePeriod}:{Constants.Variables.TimeUnit}:month]")
-               .WithOptionalParameter(ParameterBuilder.Of(Constants.Variables.TimeUnit, Entity.DatePeriod))
-               .WithOptionalParameter(ParameterBuilder.Of(Constants.Variables.TimeUnitOriginal, Entity.DatePeriod, Constants.Variables.TimeUnit))
-               .FulfillWithWebhook()
-               .Build();
+                .TriggerOn("Get revenue")
+                .TriggerOn("Get my revenue")
+                .TriggerOn("How much revenue did we generate")
+                .TriggerOn("How much money have we made")
+                .TriggerOn("How much money did we make")
+                .TriggerOn("How much were we paid")
+                .TriggerOn($"Get revenue from [{Entity.DatePeriod}:{Constants.Variables.TimeUnit}:month]")
+                .TriggerOn($"Get revenue in [{Entity.DatePeriod}:{Constants.Variables.TimeUnit}:month]")
+                .TriggerOn($"How much money have we made in [{Entity.DatePeriod}:{Constants.Variables.TimeUnit}:month]?")
+                .TriggerOn($"How much money did we make last [{Entity.DatePeriod}:{Constants.Variables.TimeUnit}:month]?")
+                .TriggerOn($"How much were we paid in [{Entity.DatePeriod}:{Constants.Variables.TimeUnit}:month]?")
+                .WithOptionalParameter(ParameterBuilder.Of(Constants.Variables.TimeUnit, Entity.DatePeriod))
+                .WithOptionalParameter(ParameterBuilder.Of(Constants.Variables.TimeUnitOriginal, Entity.DatePeriod, Constants.Variables.TimeUnit))
+                .FulfillWithWebhook()
+                .Build();
         }
     }
 }

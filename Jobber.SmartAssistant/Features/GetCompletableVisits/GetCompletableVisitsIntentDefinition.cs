7369f9579358b7919c327d28c7b81ee58e567763
George Antonious <george@antonious.ca>
2018-04-03 12:13:35 -0600

    Add more trigger statements for getting revenue and completable visits

--- Jobber.SmartAssistant/Features/GetCompletableVisits/GetCompletableVisitsIntentDefinition.cs
+++ Jobber.SmartAssistant/Features/GetCompletableVisits/GetCompletableVisitsIntentDefinition.cs
@@ -11,9 +11,16 @@ namespace Jobber.SmartAssistant.Features.GetCompletableVisits
             return IntentBuilder.For(Constants.Intents.GetCompletableVisits)
                 .TriggerOn("How many visits can be marked as complete?")
                 .TriggerOn("Completable visits")
+                .TriggerOn("Get completable visits")
+                .TriggerOn("Could you get completable visits?")
                 .TriggerOn("How many visits can I complete")
                 .TriggerOn("How many visits need to be completed")
                 .TriggerOn("How many visits are ready for completion")
+                .TriggerOn("How many visits are done")
+                .TriggerOn("Tell me how many visits are done")
+                .TriggerOn("Can you tell me how many visits are done?")
+                .TriggerOn("Can you tell me how many visits can be marked as completed?")
+                .TriggerOn("Could you tell me how many visits are done?")
                 .FulfillWithWebhook()
                 .Build();
         }

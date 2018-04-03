84a436e6b5e034990c2d5eee976e359fba273289
George Antonious <george@antonious.ca>
2018-04-03 14:09:10 -0600

    Add statments for sendable invoices

--- Jobber.SmartAssistant/Features/SendableInvoices/SendableInvoicesIntentDefinition.cs
+++ Jobber.SmartAssistant/Features/SendableInvoices/SendableInvoicesIntentDefinition.cs
@@ -9,12 +9,20 @@ namespace Jobber.SmartAssistant.Features.SendableInvoices
         public Intent DefineIntent()
         {
             return IntentBuilder.For(Constants.Intents.SendableInvoices)
-               .TriggerOn("How many invoices are ready to be sent?")
-               .TriggerOn("Ready invoices?")
-               .TriggerOn("Sendable invoices")
-               .TriggerOn("Issuable invoices")
-               .FulfillWithWebhook()
-               .Build();
+                .TriggerOn("How many invoices are ready to be sent?")
+                .TriggerOn("Can you tell me how many invoices are ready to be sent?")
+                .TriggerOn("Could you tell me how many invoices are ready to be sent?")
+                .TriggerOn("Get ready invoices")
+                .TriggerOn("Can you tell me many invoices can be sent?")
+                .TriggerOn("Could you tell me many invoices can be sent?")
+                .TriggerOn("How many invoices can be sent?")
+                .TriggerOn("How many invoices need to be sent?")
+                .TriggerOn("How many invoices need to be sent out?")
+                .TriggerOn("Ready invoices?")
+                .TriggerOn("Sendable invoices")
+                .TriggerOn("Issuable invoices")
+                .FulfillWithWebhook()
+                .Build();
         
         }
     }
